0a5a3a0830ad335a078809d1ce84fd59d9e80e09
premiash@gmail.com <premiash@gmail.com>
2017-04-29 21:20:58 -0400

    test1

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@
 var mysql = require("mysql");
 
 if(process.env.JAWSDB_URL) {
+  console.log("JawsDB" + process.env.JAWSDB_URL)
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 } else
