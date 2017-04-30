36de83c138282afc43f0e22c2d237911361ceb5f
premiash@gmail.com <premiash@gmail.com>
2017-04-29 21:17:15 -0400

    jawsdb changed

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "manuela321",
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else
+{
+  var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "manuela321",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
